1d7b015dcbd8b805b64e1356a103c6ae789cd848
Joel Fernandes <joel.fernandes@kilowott.com>
2021-12-06 15:14:21 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EMICalc
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var Object = new Result();
+            double emi;
+        try { 
+                Console.Write("Enter the price of vehicle: ");
+                double P = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter the rate of interest: ");
+                double i = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter the loan duration: ");
+                double t = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Monthly compounding: ");
+                double n = Convert.ToDouble(Console.ReadLine());
+
+                //double Result = Object.EMI(P,i,n,t);
+                emi = Object.EMI(P, i, t, n);
+                Console.WriteLine($" Monthly EMI =" + emi);
+            }
+
+            catch (FormatException e)
+            {
+                Console.WriteLine("invalid input");
+            }
+        }
+        
+    }
+}
---
+++ Result.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EMICalc
+{
+    internal class Result
+    {
+        public double EMI(double P, double i, double t, double n)
+        {
+            
+            //i = i / 100;
+             //emi = (P * Math.Pow(1 + i/n ,t*n));
+            //double r = m / 24;
+            double emi = (P * Math.Pow((1 + i / n), (t * n)))/24;
+
+            return emi;
+        }
+        
+    }
+}
