de2e8a34769f4157fce2614510174e1b7beec415
joel-fernandes-kilowott <92839656+joel-fernandes-kilowott@users.noreply.github.com>
2021-12-07 10:35:52 +0530

    Add files via upload

---
+++ EmiApp.Cmdline/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace EmiApp.Cmdline
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var emiRequest = new Request() { Principal = 100000, InterestRateInPercentage = 12, LoanDurationInYearCount = 2 };
+            ICalculator emiCalculator = new Calculator();
+
+            #region monthly compounding
+            var emiMonthly = emiCalculator.CalculateMonthlyEmi(emiRequest);
+            
+            if (string.IsNullOrEmpty(emiMonthly.ErrorMessage))
+                Console.WriteLine($"Monthly EMI is {emiMonthly.EmiPayment}");
+            else 
+                Console.WriteLine(emiMonthly.ErrorMessage);
+            #endregion 
+
+            #region daily compounding
+            var emiDaily = emiCalculator.CalculateDailyEmi(emiRequest);
+
+            if (string.IsNullOrEmpty(emiDaily.ErrorMessage))
+                Console.WriteLine($"Daily EMI is {emiDaily.EmiPayment}");
+            else
+                Console.WriteLine(emiDaily.ErrorMessage);
+            #endregion 
+
+            #region continous compounding
+            var emiContinous = emiCalculator.CalculateContinousEmi(emiRequest);
+
+            if (string.IsNullOrEmpty(emiDaily.ErrorMessage))
+                Console.WriteLine($"Continous EMI is {emiContinous.EmiPayment}");
+            else
+                Console.WriteLine(emiContinous.ErrorMessage);
+            #endregion 
+
+            // fill other regions below...
+
+        }
+    }
+}
+
