6782506fdccdf830f7a42813765a801eb8d6ca14
joel-fernandes-kilowott <92839656+joel-fernandes-kilowott@users.noreply.github.com>
2021-12-10 01:14:33 +0530

    Add files via upload

---
+++ Controllers/EmiContinousController.cs
@@ -0,0 +1,29 @@
+﻿using EmiMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmiMvc.Controllers
+{
+    public class EmiContinousController : Controller
+    {
+        public IActionResult Output(double principal, double rate, double duration)
+        {
+            double Result;
+            Request request = new Request();
+            request.Principal = principal;
+            request.InterestRateInPercentage = rate;
+            request.LoanDurationInYearCount = duration;
+            double e = 2.7183;
+            Result = (double)request.Principal * System.Math.Pow(e, ((request.InterestRateInPercentage / 100) * request.LoanDurationInYearCount));
+            request.Result = Math.Round(Result, 2);
+            return View(request);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult EmiDaily()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Request.cs
@@ -0,0 +1,11 @@
+﻿namespace EmiMvc.Models
+{
+    public class Request
+    {
+        public double Principal { internal get; set; }
+        public double InterestRateInPercentage { internal get; set; }
+        public double LoanDurationInYearCount { internal get; set; }
+        public double Result { internal get; set; }
+    }
+
+}
