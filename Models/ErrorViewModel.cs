85c30b5eb6fbc8aee4ba6a692a4db6b35708d76e
joel-fernandes-kilowott <92839656+joel-fernandes-kilowott@users.noreply.github.com>
2021-12-10 10:05:13 +0530

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EmiMvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
